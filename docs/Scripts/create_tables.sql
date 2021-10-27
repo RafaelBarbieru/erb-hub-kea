713bb4a67d4167c2bd38f6bc7d6d0e8c80b4fa0c
RafaelCristianBarbieru <baracri00@gmail.com>
2021-10-27 16:17:16 +0200

    Updates some database scripts

--- docs/Scripts/create_tables.sql
+++ docs/Scripts/create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS	`battles`(
 
 	`id` int NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
-	`duration` char(8) NOT NULL,
+	`duration` int NOT NULL,
 	`publication_date` date NOT NULL,
 	`lyrics` text	NOT NULL,
 	`youtube_link` varchar(255) DEFAULT NULL,
@@ -32,12 +32,12 @@ CREATE TABLE IF NOT EXISTS `characters` (
 
 CREATE TABLE IF NOT EXISTS `battles_characters` (
 
-	`battles_id` int NOT NULL,
-	`characters_id` int NOT NULL,
+	`battle_id` int NOT NULL,
+	`character_id` int NOT NULL,
 	
-	PRIMARY KEY (`battles_id`,`characters_id`),
-	CONSTRAINT `fk_battle_id` FOREIGN KEY (`battles_id`) REFERENCES `battles` (`id`) ON UPDATE CASCADE,
-	CONSTRAINT `fk_character_id` FOREIGN KEY (`characters_id`) REFERENCES `characters` (`id`) ON UPDATE CASCADE
+	PRIMARY KEY (`battle_id`,`character_id`),
+	CONSTRAINT `fk_battle_id` FOREIGN KEY (`battle_id`) REFERENCES `battles` (`id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_character_id` FOREIGN KEY (`character_id`) REFERENCES `characters` (`id`) ON UPDATE CASCADE
 	
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
@@ -57,12 +57,12 @@ CREATE TABLE IF NOT EXISTS `actors` (
 
 CREATE TABLE IF NOT EXISTS `actors_characters` (
 
-	`actors_id` int NOT NULL ,
-	`characters_id` int NOT NULL ,	 
+	`actor_id` int NOT NULL ,
+	`character_id` int NOT NULL ,	 
 	
-	PRIMARY KEY (`actors_id`,`characters_id`),	 
-	CONSTRAINT `fk_actor_id` FOREIGN KEY (`actors_id`) REFERENCES `actors` (`id`),
-	CONSTRAINT `fk_character_id_ac` FOREIGN KEY (`characters_id`) REFERENCES `characters` (`id`) ON UPDATE CASCADE
+	PRIMARY KEY (`actor_id`,`character_id`),	 
+	CONSTRAINT `fk_actor_id` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`),
+	CONSTRAINT `fk_character_id_ac` FOREIGN KEY (`character_id`) REFERENCES `characters` (`id`) ON UPDATE CASCADE
 	
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
@@ -79,13 +79,13 @@ CREATE TABLE IF NOT EXISTS `social_media` (
 
 CREATE TABLE IF NOT EXISTS `actors_social_media` (
 
-	`actors_id` int NOT NULL ,
+	`actor_id` int NOT NULL ,
 	`social_media_id` int NOT NULL ,
 	`link` varchar(255) NULL,
 		 
-	PRIMARY KEY (`actors_id`,`social_media_id`),
+	PRIMARY KEY (`actor_id`,`social_media_id`),
 	
-	CONSTRAINT `fk_actor_id_asm` FOREIGN KEY (`actors_id`) REFERENCES `actors` (`id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_actor_id_asm` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`) ON UPDATE CASCADE,
 	CONSTRAINT `fk_social_id` FOREIGN KEY (`social_media_id`) REFERENCES `social_media` (`id`) ON UPDATE CASCADE
 	
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
--- docs/Scripts/populate/populate_actors_social_media.sql
+++ docs/Scripts/populate/populate_actors_social_media.sql
@@ -1,20 +1,20 @@
 USE `erb_hub`;
 
 -- Nice Peter - YouTube
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://www.youtube.com/user/nicepeter', 1, 1);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://www.youtube.com/user/nicepeter', 1, 1);
 -- Nice Peter - Twitter
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://twitter.com/nicepeter', 1, 2);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://twitter.com/nicepeter', 1, 2);
 -- Nice Peter - Spotify
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://open.spotify.com/artist/5qcFSzd44Md3Ub2tNngTte?si=2ve2LlSUT_Wqup7lCXQspA&dl_branch=1', 1, 3);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://open.spotify.com/artist/5qcFSzd44Md3Ub2tNngTte?si=2ve2LlSUT_Wqup7lCXQspA&dl_branch=1', 1, 3);
 -- EpicLLOYD - YouTube
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://www.youtube.com/user/EPICLLOYD', 2, 1);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://www.youtube.com/user/EPICLLOYD', 2, 1);
 -- EpicLLOYD - Twitter
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://twitter.com/TheEpicLLOYD', 2, 2);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://twitter.com/TheEpicLLOYD', 2, 2);
 -- EpicLLOYD - Spotify
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://open.spotify.com/artist/62sK16KPm7eBSUDWcINUd3?si=qeWqat1ORIuWnLqQzfQ44A&dl_branch=1', 2, 3);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://open.spotify.com/artist/62sK16KPm7eBSUDWcINUd3?si=qeWqat1ORIuWnLqQzfQ44A&dl_branch=1', 2, 3);
 -- Boyinaband - YouTube
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://www.youtube.com/c/boyinaband', 3, 1);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://www.youtube.com/c/boyinaband', 3, 1);
 -- Boyinaband - Twitter
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://twitter.com/davepbrown', 3, 2);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://twitter.com/davepbrown', 3, 2);
 -- Boyinaband - Spotify
-INSERT INTO `actors_social_media`(link, actors_id, social_media_id) VALUES('https://open.spotify.com/artist/7kfzadZaOgR90Tp8OYH5gM?si=DUhsSR1RQDaDVQd4TaIaHA&dl_branch=1', 3, 3);
+INSERT INTO `actors_social_media`(link, actor_id, social_media_id) VALUES('https://open.spotify.com/artist/7kfzadZaOgR90Tp8OYH5gM?si=DUhsSR1RQDaDVQd4TaIaHA&dl_branch=1', 3, 3);
